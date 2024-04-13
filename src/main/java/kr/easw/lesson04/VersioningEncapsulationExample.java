dbc7b25c37e28157fd577889d9952dad08493987
Myung Ji Sung <mjs3738@naver.com>
2024-04-13 16:49:55 +0900

    04/13 자바프로그래밍 4번째 과제 2021039036 명지성

--- src/main/java/kr/easw/lesson04/VersioningEncapsulationExample.java
+++ src/main/java/kr/easw/lesson04/VersioningEncapsulationExample.java
@@ -1,10 +1,7 @@
 package kr.easw.lesson04;
-
 /**
  * 해당 클래스는 {@link TestInterface}를 상속받아 새 버전을 구현하는 코드를 가지고 있습니다.
- *
  * 해당 문제에서는 {@link TestV1}, {@link TestV2}, {@link TestV3} 3개의 클래스를 수정해 정상적인 값을 도출하도록 해야 합니다.
- *
  * 이는 다음의 조건을 따라야 합니다 :
  * - TestV1 클래스는 입력된 문자열을 그대로 반환해야 합니다.
  * - TestV2 클래스는 입력된 문자열에서 느낌표(!)를 물음표(?)로 바꿔야 합니다.
@@ -12,11 +9,8 @@ package kr.easw.lesson04;
  */
 public class VersioningEncapsulationExample {
     public static String VALUE = "Hello, World!";
-
     public static String RESULT_SECOND = "Hello, World?";
-
     public static String RESULT_THIRD = "Hello,World?";
-
     public static void main(String[] args) {
         TestInterface test = new TestV1();
         if (!test.doAction(VALUE).equals(VALUE)) {
@@ -35,34 +29,27 @@ public class VersioningEncapsulationExample {
         }
         System.out.println("정답입니다.");
     }
-
     interface TestInterface {
         String doAction(String type);
     }
-
-
     static class TestV1 implements TestInterface {
         @Override
         public String doAction(String type) {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+                    return type;
         }
     }
-
-
     // Split by space
     static class TestV2 implements TestInterface {
         @Override
         public String doAction(String type) {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+            return type.replace('!','?');
         }
     }
-
-
     // Split by space, and concat
     static class TestV3 implements TestInterface {
         @Override
         public String doAction(String type) {
-            throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+            return type.replace('!', '?').replace(" ", "");
         }
     }
 }
